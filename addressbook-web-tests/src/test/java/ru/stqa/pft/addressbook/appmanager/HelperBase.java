2235aac31b385ad9d0cc13e481d89151e9eded53
Salarus1488 <damir.yusupov.1992@gmail.com>
2019-03-18 09:40:13 +0300

    Lesson 3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -17,8 +17,13 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
     }
 
     public boolean isAlertPresent() {
@@ -29,6 +34,7 @@ public class HelperBase {
             return false;
         }
     }
+
     public void acceptAllert() {
         wd.switchTo().alert().accept();
     }
