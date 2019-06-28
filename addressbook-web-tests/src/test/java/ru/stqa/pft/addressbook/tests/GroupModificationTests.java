97a139cff0b7004bf127e3c93c7a3cfaad3540a2
Salarus1488 <damir.yusupov.1992@gmail.com>
2019-06-28 12:20:33 +0300

    Lesson 7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -35,7 +35,10 @@ public class GroupModificationTests extends TestBase {
         assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.db().groups();
         assertThat(after, CoreMatchers.equalTo(before.without(modifiedGroup).withAdded(group)));
+        verifyGroupListUI();
     }
+
+
 
 
 }
